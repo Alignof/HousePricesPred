26a77fbadd1729afb9d48ad54d56f67e31636580
Alingof <Alignof@outlook.com>
2022-01-04 23:54:13 +0900

    [add] add the function csv_to_df()

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,19 @@ use polars::prelude::*;
 use smartcore::linalg::naive::dense_matrix::DenseMatrix;
 use smartcore::linear::ridge_regression::{RidgeRegression, RidgeRegressionParameters};
 
-fn load_df() -> Result<(DataFrame, DataFrame)> {
-    let test_path = "data/test.csv";
-    let df_test = CsvReader::from_path(test_path)?
-        .infer_schema(None)
-        .with_delimiter(b',')
-        .has_header(true)
-        .finish()?;
-
-    let train_path = "data/train.csv";
-    let df_train = CsvReader::from_path(train_path)?
-        .infer_schema(None)
-        .with_delimiter(b',')
-        .has_header(true)
-        .finish()?;
-
-    Ok((df_train, df_test))
+fn csv_to_df(file_path: &str) -> Result<DataFrame> {
+    Ok(
+        CsvReader::from_path(file_path)?
+            .infer_schema(None)
+            .with_delimiter(b',')
+            .has_header(true)
+            .finish()?
+    )
 }
 
 fn main() -> Result<()> {
-    let (df_train, df_test) = load_df().expect("load csv error.");
+    let df_test = csv_to_df("data/test.csv")?;
+    let df_train = csv_to_df("data/train.csv")?;
     let selected_feature = vec![
         "LotArea",
         "OverallQual",
