ef54283b2eb7ab65ebede2882553dda471d772b5
Alingof <Alignof@outlook.com>
2022-01-04 23:48:34 +0900

    [update] update to read data/test.csv

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,62 @@
 use polars::prelude::*;
 use smartcore::linalg::naive::dense_matrix::DenseMatrix;
 use smartcore::linear::ridge_regression::{RidgeRegression, RidgeRegressionParameters};
-use smartcore::model_selection::train_test_split;
-use smartcore::metrics::mean_squared_error;
 
-fn main() -> Result<()> {
-    let train_path = "data/train.csv";
-    let df = CsvReader::from_path(train_path)?
+fn load_df() -> Result<(DataFrame, DataFrame)> {
+    let test_path = "data/test.csv";
+    let df_test = CsvReader::from_path(test_path)?
         .infer_schema(None)
         .with_delimiter(b',')
         .has_header(true)
         .finish()?;
 
-    let feature_df = df.select(
-        vec![
-            "LotArea",
-            "OverallQual",
-            "OverallCond",
-            "TotalBsmtSF",
-            "1stFlrSF",
-            "GarageCars",
-        ]        
-    )?;
+    let train_path = "data/train.csv";
+    let df_train = CsvReader::from_path(train_path)?
+        .infer_schema(None)
+        .with_delimiter(b',')
+        .has_header(true)
+        .finish()?;
+
+    Ok((df_train, df_test))
+}
+
+fn main() -> Result<()> {
+    let (df_train, df_test) = load_df().expect("load csv error.");
+    let selected_feature = vec![
+        "LotArea",
+        "OverallQual",
+        "OverallCond",
+        "TotalBsmtSF",
+        "1stFlrSF",
+        "GarageCars",
+    ];
+
+    let feature = df_train.select(&selected_feature)?;
     let feature = DenseMatrix::from_vec(
-        feature_df.height(),
-        feature_df.width(),
-        &feature_df.to_ndarray::<Float64Type>()?.into_raw_vec()
+        feature.height(),
+        feature.width(),
+        &feature.to_ndarray::<Float64Type>()?.into_raw_vec()
     );
-    let target = df.select(vec!["Id"])?
+    let target = df_train.select(vec!["SalePrice"])?
         .to_ndarray::<Float64Type>()?
         .into_raw_vec();
 
-    let (f_train, f_test, t_train, t_test) = train_test_split(&feature, &target, 0.2, true);
+    let feature_test = df_test.select(&selected_feature)?;
+    let feature_test = DenseMatrix::from_vec(
+        feature_test.height(),
+        feature_test.width(),
+        &feature_test.to_ndarray::<Float64Type>()?.into_raw_vec()
+    );
 
     let rr_predicted = RidgeRegression::fit(
-        &f_train,
-        &t_train,
+        &feature,
+        &target,
         RidgeRegressionParameters::default().with_alpha(0.8),
     )
-    .and_then(|rr| rr.predict(&f_test))
+    .and_then(|rr| rr.predict(&feature_test))
     .unwrap();
 
     dbg!(&rr_predicted);
-
-    println!(
-        "RMSE Ridge Regression: {}",
-        mean_squared_error(&t_test, &rr_predicted).cbrt()
-    );
 
     Ok(())
 }
