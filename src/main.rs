a83f4f89eac324a95ebd2714726595ae470037da
Alingof <Alignof@outlook.com>
2022-01-05 15:55:20 +0900

    [refactor] apply formatter and linter

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,32 @@
-use std::io::Write;
-use std::fs::File;
 use polars::prelude::*;
 use smartcore::linalg::naive::dense_matrix::DenseMatrix;
 use smartcore::linear::ridge_regression::{RidgeRegression, RidgeRegressionParameters};
+use std::fs::File;
+use std::io::Write;
 
 fn csv_to_df(file_path: &str) -> Result<DataFrame> {
-    Ok(
-        CsvReader::from_path(file_path)?
-            .infer_schema(None)
-            .with_delimiter(b',')
-            .has_header(true)
-            .finish()?
-    )
+    CsvReader::from_path(file_path)?
+        .infer_schema(None)
+        .with_delimiter(b',')
+        .has_header(true)
+        .finish()
 }
 
 fn df_to_dm(df: &DataFrame) -> Result<DenseMatrix<f64>> {
-    let feature = df.select(
-        vec![
-            "LotArea",
-            "OverallQual",
-            "OverallCond",
-            "TotalBsmtSF",
-            "1stFlrSF",
-            "GarageCars",
-        ]
-    )?;
+    let feature = df.select(vec![
+        "LotArea",
+        "OverallQual",
+        "OverallCond",
+        "TotalBsmtSF",
+        "1stFlrSF",
+        "GarageCars",
+    ])?;
 
-    Ok(
-        DenseMatrix::from_vec(
-            feature.height(),
-            feature.width(),
-            &feature.to_ndarray::<Float64Type>()?.into_raw_vec()
-        )
-    )
+    Ok(DenseMatrix::from_vec(
+        feature.height(),
+        feature.width(),
+        &feature.to_ndarray::<Float64Type>()?.into_raw_vec(),
+    ))
 }
 
 fn save_predict(ids: Vec<f64>, pred: Vec<f64>) {
@@ -50,19 +44,21 @@ fn main() -> Result<()> {
     let df_train = csv_to_df("data/train.csv")?;
 
     let feature = df_to_dm(&df_train)?;
-    let target = df_train.select(vec!["SalePrice"])?
+    let target = df_train
+        .select(vec!["SalePrice"])?
         .to_ndarray::<Float64Type>()?
         .into_raw_vec();
 
     let test_feat = df_to_dm(&df_test)?;
-    let test_ids = df_test.select(vec!["Id"])?
+    let test_ids = df_test
+        .select(vec!["Id"])?
         .to_ndarray::<Float64Type>()?
         .into_raw_vec();
 
     let rr_predicted = RidgeRegression::fit(
         &feature,
         &target,
-        RidgeRegressionParameters::default().with_alpha(0.5),
+        RidgeRegressionParameters::default().with_alpha(1.9),
     )
     .and_then(|rr| rr.predict(&test_feat))
     .unwrap();
@@ -71,4 +67,3 @@ fn main() -> Result<()> {
 
     Ok(())
 }
-
